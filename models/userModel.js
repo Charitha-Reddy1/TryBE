c361cb761cc9434bfdef7618bffb57d227415bb0
Charitha-Reddy1 <carg2800@gmail.com>
2026-05-17 12:17:53 +0530

    namefasak

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const userSchema = mongoose.Schema({
-  name:{type:String},
+  
   email: { type: String, required: true,unique:true },
   password: { type: String, required: true },
 });
